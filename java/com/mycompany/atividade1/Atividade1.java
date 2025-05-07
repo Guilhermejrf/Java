2376cc8aadff67b2f87479c3a590f34440697526
Guilhermejrf <164170606+Guilhermejrf@users.noreply.github.com>
2025-05-06 22:20:23 -0300

    Add files via upload

---
+++ java/com/mycompany/atividade1/Atividade1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade1;
+
+/**
+ *
+ * @author GAMER
+ */
+public class Atividade1 {
+
+    public static void main(String[] args) {
+        System.out.println("uma mensagem diferente " + " uma outra mensagem diferente");
+    }
+}
